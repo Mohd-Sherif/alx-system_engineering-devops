785b3e79f7f8de99f363370b584537695d2a76d4
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-07-03 11:32:37 -0700

    install Werkzeug too

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,25 @@
 # install flask from pip3
 
-package { 'flask':
+#package { 'flask':
+#  ensure   => '2.1.0',
+#  provider => 'pip3',
+#}
+
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => present,
+}
+
+# Install Werkzeug version compatible with Flask 2.1.0
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+# Install Flask version 2.1.0 using pip3
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
